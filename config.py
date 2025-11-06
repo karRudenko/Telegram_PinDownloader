9fae9dcee5e1f04921033c8cc2b50a4bd1b0800f
Karyna Rudenko <1karrak2karrak3karrak@gmail.com>
2025-11-06 22:39:49 +0100

    Change getenv to environ.get for token retrieval

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("YOUR_TOKEN")
-RAPID_API_KEY = os.getenv("YOUR_KEY")
+BOT_TOKEN = os.environ.get('YOUR_TOKEN', '')
+RAPID_API_KEY = os.environ.get('YOUR_KEY', '')
 URL = "https://pinterest-video-and-image-downloader.p.rapidapi.com/pinterest"
 
 headers = {
